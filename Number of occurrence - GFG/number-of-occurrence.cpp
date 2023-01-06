f7e8af20d88d115204c7c6d90e3033a038cc3799
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-01-06 23:08:31 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	
+	int startPos(int arr[], int n, int x){
+	    int ans = -1;
+	    int left = 0;
+	    int right = n -1;
+	    
+	    while(left <= right){
+	        int mid = left + (right - left)/2;
+	        
+	        if(arr[mid] == x){
+	            ans = mid;
+	            right = mid - 1;
+	        }
+	        else if(x > arr[mid]){
+	            left = mid + 1;
+	        }
+	        else{
+	            right = mid - 1;
+	        }
+	    }
+	    
+	    return ans;
+	    
+	}
+	
+	int endPos(int arr[], int n, int x){
+	    int ans = -1;
+	    int left = 0;
+	    int right = n -1;
+	    
+	    while(left <= right){
+	        int mid = left + (right - left)/2;
+	        
+	        if(arr[mid] == x){
+	            ans = mid;
+	            left = mid + 1;
+	        }
+	        else if(x > arr[mid]){
+	            left = mid + 1;
+	        }
+	        else{
+	            right = mid - 1;
+	        }
+	    }
+	    
+	    return ans;
+	    
+	}
+	
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int first = startPos(arr, n, x);
+	    int last = endPos(arr, n, x);
+	    
+	    if(first == -1 || last == -1){
+	        return 0;
+	    }
+	    else{
+	        return last - first + 1;
+	    }
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
